e46827ffd8aca0da29ca91a5a4cec5a1934d0d14
Francisco Javier Garcia Orduna <francisco.orduna@king.com>
2018-12-13 08:59:23 +0100

    step 16

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/bean/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/bean/ExchangeValue.java
@@ -1,12 +1,34 @@
 package com.in28minutes.microservices.currencyexchangeservice.bean;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Transient;
 
+@Entity
 public class ExchangeValue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "abstract_generator")
+    @SequenceGenerator(name = "abstract_generator", sequenceName = "exchangevalue_seq", allocationSize = 1)
     private Long id;
+
+    @Column(nullable = false, name = "currency_from")
     private String from;
+
+    @Column(nullable = false, name = "currency_to")
     private String to;
+
+    @Column(nullable = false)
     private BigDecimal conversionMultiple;
+
+    @JsonInclude
+    @Transient
     private int port;
 
     protected ExchangeValue() {
