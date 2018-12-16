8aa8b5eeb5e8438db0503c53dcec1a5f07cd626e
Francisco Javier Garcia Orduna <francisco.orduna@king.com>
2018-12-16 13:49:45 +0100

    step 32

---
+++ netfix-zuul-api-gateway-server/src/main/java/com/in28minutes/microservices/netfixzuulapigatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,40 @@
+package com.in28minutes.microservices.netfixzuulapigatewayserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        logger.info("request -> {} request uri -> {}", request, request.getRequestURI());
+        return null;
+    }
+}
